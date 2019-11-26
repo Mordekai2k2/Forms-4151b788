a1b765b68a93636200ee0dfc0d4976fb13e5db22
Randy Brouwer <2105070@talnet.nl>
2019-11-26 11:41:47 +0100

    Uitwerking van de exercise

---
+++ form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+</head>
+<body>
+    <h1>
+        <?php
+            echo "Believe it or not, HTML IS BACK BABY!!!"
+        ?>
+    </h1>
+    <form>
+        <input type="text">
+        <input type="submit">
+    </form>
+</body>
